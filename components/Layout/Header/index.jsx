43054dc45a251c77c65acffd98fd7009bc7b4755
Lund vaja <jepoma1923@about27.com>
2022-06-01 18:55:02 +0000

    init

--- components/Layout/Header/index.jsx
+++ components/Layout/Header/index.jsx
@@ -7,13 +7,10 @@ import { useRouter } from "next/router";
 import DeskTopMenu from "./DeskTopMenu";
 import AuthBtn from "./AuthBtn";
 import Close from "components/icons/Close";
-import useMediaQuery from "lib/useMediaQuery";
 
 export default function NavBar() {
   const [open, setOpen] = useState(false);
   const { pathname } = useRouter();
-  const isDesktop = useMediaQuery("(min-width: 768px)");
-  const isMobile = useMediaQuery("(max-width: 768px)");
 
   const navOpen = () => {
     setOpen(true);
@@ -30,37 +27,31 @@ export default function NavBar() {
   return (
     <header className="border-b">
       <nav className="navbar container max-w-[50rem] mx-auto px-4 flex filter bg-white h-[56px] items-center justify-between">
-        {isMobile && (
-          <div className="hamburger-button">
-            {open ? (
-              <Close iconClass="h-5 w-5 text-gray-500" setOpen={navClose} />
-            ) : (
-              <Menu iconClass="h-5 w-5 text-gray-500" setOpen={navOpen} />
-            )}
-          </div>
-        )}
+        <div className="hamburger-button md:hidden">
+          {open ? (
+            <Close iconClass="h-5 w-5 text-gray-500" setOpen={navClose} />
+          ) : (
+            <Menu iconClass="h-5 w-5 text-gray-500" setOpen={navOpen} />
+          )}
+        </div>
         <Logo />
-        {isMobile && (
-          <ul
-            className={`mobile-menu z-50 overflow-hidden absolute top-[57px] p-4 left-0 right-0 bg-white transition-transform ease-[cubic-bezier(1, 0.5, 1, 0.25))] ${
-              open
-                ? "h-auto translate-y-0 visible border-b"
-                : "h-0 translate-y-1 invisible"
-            }`}
-          >
-            {open &&
-              NavData.map((navdata, index) => {
-                return <MobileMenu key={index} {...navdata} />;
-              })}
-          </ul>
-        )}
-        {isDesktop && (
-          <ul className="desktop-menu inline-flex">
-            {NavData.map((navdata, index) => {
-              return <DeskTopMenu key={index} {...navdata} />;
+        <ul
+          className={`mobile-menu md:hidden z-50 overflow-hidden absolute top-[57px] p-4 left-0 right-0 bg-white transition-transform ease-[cubic-bezier(1, 0.5, 1, 0.25))] ${
+            open
+              ? "h-auto translate-y-0 visible border-b"
+              : "h-0 translate-y-1 invisible"
+          }`}
+        >
+          {open &&
+            NavData.map((navdata, index) => {
+              return <MobileMenu key={index} {...navdata} />;
             })}
-          </ul>
-        )}
+        </ul>
+        <ul className="desktop-menu hidden md:flex">
+          {NavData.map((navdata, index) => {
+            return <DeskTopMenu key={index} {...navdata} />;
+          })}
+        </ul>
         <AuthBtn />
       </nav>
     </header>
